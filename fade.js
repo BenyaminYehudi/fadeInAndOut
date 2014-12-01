dcd46e2de2dafb84550c7522c155ed2fd7e6ac0d
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-12-01 20:38:20 +0200

    Update fade.js

--- fade.js
+++ fade.js
@@ -8,19 +8,19 @@ $(document).ready(function(){
   });
 
   $("button:nth(1)").on("click", function(){
-   $("button:nth(1)").fadeOut(5555);
+   $("button:nth(1)").fadeOut(7654);
   });
   
   $("button:nth(2)").on("click", function(){
-   $("button:nth(2)").fadeOut(1111);
+   $("button:nth(2)").fadeOut(6543);
   });
 
   $("button:nth(3)").on("click", function(){
-    $("button:nth(3)").fadeOut(888);
+    $("button:nth(3)").fadeOut(5432);
     });
     
     $("button:nth(4)").on("click", function(){
-   $("button:nth(4)").fadeOut(9876);
+   $("button:nth(4)").fadeOut(4321);
   });
 
 });
